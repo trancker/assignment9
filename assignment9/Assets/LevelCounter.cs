4e939d43b0b9fdbb8e19e4326ea57496f3c55ff2
tanzil786 <tanzilidrisi68@gmail.com>
2020-09-12 15:46:05 +0530

    Initial commit

---
+++ assignment9/Assets/LevelCounter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelCounter : MonoBehaviour {
+
+	public static int level = 1;
+	public Text leveler;
+	
+	// Use this for initialization
+	void Start () {
+		leveler.text = "LEVEL " + level;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ assignment9/Assets/Scripts/DespawnOnHeight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DespawnOnHeight : MonoBehaviour {
+
+	public GameObject characterController;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (characterController.transform.position.y < -10)
+		{
+			Destroy(GameObject.Find("WhisperSource"));
+			LevelCounter.level = 1;
+			SceneManager.LoadScene("End");
+		}
+	}
+}
